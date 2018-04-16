f02111df735cf953db7f8fb2e26c54f0d7210f25
Jonathan Rupp <jrupp@rightpoint.com>
2018-04-16 15:43:35 -0500

    Get things working with log4net versions

---
+++ BindingRedirect.Child/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace BindingRedirect.Child
+{
+    public class Class1
+    {
+        public string GetInfo()
+        {
+            return typeof(LogManager).AssemblyQualifiedName;
+        }
+    }
+}
--- BindingRedirect.Parent/Program.cs
+++ BindingRedirect.Parent/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BindingRedirect.Child;
+using log4net;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,29 @@ namespace BindingRedirect.Parent
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine($"Parent: {GetTypeFromParent()}");
+                Console.WriteLine($"Child: {GetTypeFromChild()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            if (Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetTypeFromParent()
+        {
+            return typeof(LogManager).AssemblyQualifiedName;
+        }
+
+        private static string GetTypeFromChild()
+        {
+            return new Class1().GetInfo();
         }
     }
 }
